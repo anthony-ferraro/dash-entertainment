54668bf21caca9ef45f4ce8f4617cd3d03d2dd84
Anthony Ferraro <aferaro525@gmail.com>
2022-04-17 23:52:20 -0400

    Fixed prop capitalization errors preventing npm build from completing

--- pages/details/movie/[id].js
+++ pages/details/movie/[id].js
@@ -36,12 +36,12 @@ const _id_ = ({ router }) => {
     return (
 
       <>
-        <iframe width="560" height="315" src={`https://www.youtube.com/embed/${videoKey}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" src={`https://www.youtube.com/embed/${videoKey}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         {/* <p className="c-white">{JSON.stringify()}</p> */}
         {/* <p className="c-white">{providerString}</p> */}
         
         {/* list of provider categories (rent, buy, flatrate)*/}
-        <ul>
+        {/* <ul>
           {!!providerCategories && providerCategories.map(providerCategory =>
             <>
               <li className="c-red">{providerCategory}</li>
@@ -52,7 +52,7 @@ const _id_ = ({ router }) => {
               </li>
             </>
           )}
-        </ul>
+        </ul> */}
       </>
     )
   }
