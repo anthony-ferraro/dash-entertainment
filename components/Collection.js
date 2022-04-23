afb739a7ebede5199e9527ca54cd342ce98099b6
Anthony Ferraro <aferaro525@gmail.com>
2022-04-23 14:29:55 -0400

    Added morePath property to Collection component

--- components/Collection.js
+++ components/Collection.js
@@ -3,7 +3,7 @@ import useSWR from 'swr'
 import { fetcher, getURL, parse } from '../utilities'
 import CollectionItem from './CollectionItem'
 import Loading from './Loading'
-const Collection = ({ type = "normal", displayCount, path, optional="", label, router}) => {
+const Collection = ({ type = "normal", displayCount, path, optional = "", label, router, morePath = "" }) => {
     const { data, error } = useSWR(getURL(path, optional), fetcher)
     const handleClick = (contentItem) => {
         router.push(`/details/${contentItem.category.toLowerCase()}/${contentItem.id}`)
@@ -22,11 +22,14 @@ const Collection = ({ type = "normal", displayCount, path, optional="", label, r
             <Loading></Loading>
         )
     } else {
-        const contentList = (displayCount===undefined || (displayCount!==undefined && displayCount>parse(data).length)) ? parse(data) : parse(data).slice(0, displayCount);
+        const contentList = (displayCount === undefined || (displayCount !== undefined && displayCount > parse(data).length)) ? parse(data) : parse(data).slice(0, displayCount);
         return (
-            <>  
-                {/* <p className="c-white">{JSON.stringify(data)}</p> */}
-                <p className="heading-L c-white">{label}</p>
+            <>
+                <div className="labels">
+                    <p className="heading-L c-white">{label}</p>
+                    {morePath!=="" && <p onClick={() => router.push(morePath)} className="body-M c-greyblue">VIEW MORE</p>}
+                </div>
+
                 <div className={type === "wide" ? "wide-list" : type === "normal" ? "content-list" : "content-list"}>
                     {contentList.map((contentItem, index) =>
                         <CollectionItem key={index} contentItem={contentItem} type={type} handleClick={handleClick}></CollectionItem>
--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,12 @@ export default function Home({ router }) {
   const displayCount = 11;
   return (
     <>
-      <Collection label="Trending Now" path={paths.trending.all} type="wide" displayCount={displayCount} router={router}></Collection>
-      <Collection label="In Theaters" path={paths.nowPlaying.movies} displayCount={displayCount} router={router}></Collection>
-      <Collection label="Popular Movies" path={paths.popular.movies} displayCount={displayCount} router={router}></Collection>
-      <Collection label="Airing Today" path={paths.nowPlaying.tv} type="wide" displayCount={displayCount} router={router}></Collection>
-      <Collection label="Popular TV Series" path={paths.popular.tv} displayCount={displayCount} router={router}></Collection>
-      <Collection label="Top Rated Movies" path={paths.topRated.movies} displayCount={displayCount} router={router}></Collection>
+      <Collection label="Trending Now" path={paths.trending.all} type="wide" displayCount={displayCount} router={router} morePath="/trending"></Collection>
+      <Collection label="In Theaters" path={paths.nowPlaying.movies} displayCount={displayCount} router={router} morePath="/movies/now-playing/"></Collection>
+      <Collection label="Popular Movies" path={paths.popular.movies} displayCount={displayCount} router={router} morePath="/movies/popular/"></Collection>
+      <Collection label="Airing Today" path={paths.nowPlaying.tv} type="wide" displayCount={displayCount} router={router} morePath="/tv/now-playing"></Collection>
+      <Collection label="Popular TV Series" path={paths.popular.tv} displayCount={displayCount} router={router} morePath="/tv/popular/"></Collection>
+      <Collection label="Top Rated Movies" path={paths.topRated.movies} displayCount={displayCount} router={router} morePath="/movies/top-rated"></Collection>
     </>
   )
 }
